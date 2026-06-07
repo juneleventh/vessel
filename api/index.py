b391267fed840cb0d431926e32dfeb1fe75d2595
juneleventh <25ds3000002@ds.study.iitm.ac.in>
2026-06-07 18:34:01 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
