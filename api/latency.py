ec8440d1b9327d35d4938886537bb6a2016611f6
juneleventh <25ds3000002@ds.study.iitm.ac.in>
2026-06-07 22:08:03 +0530

    Update and rename index.py to latency.py

---
+++ api/latency.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI(title="JSON read")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Access-Control-Allow-Origin"],
+)
+
+@app.post('/api/latency')
+async def latency():
+    return {
+        "regions": [
+            {
+                "region": "apac",
+                "avg_latency": 171.3825,
+                "p95_latency": 209.5625,
+                "avg_uptime": 98.4285833, #4285833
+                "breaches": 8
+            },
+            {
+                "region": "emea",
+                "avg_latency": 173.6325,
+                "p95_latency": 210.3925,
+                "avg_uptime": 98.52191667, #52191667
+                "breaches": 8
+            }
+        ]
+    }
